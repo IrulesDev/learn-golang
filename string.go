90ebff13f03e060c7c9cc22ccf477cdfc8e4f9ba
IrulesDev <ad4112ul@gmail.com>
2025-03-06 23:03:56 +0700

    h1 belajar go

---
+++ string.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("fauzan")
+	fmt.Println("fauzan fathoni")
+	fmt.Println("fauzan fathoni khorul")
+	fmt.Println("fauzan fathoni khorul huda")
+
+
+	fmt.Println(len("fauzan"))
+	fmt.Println("fauzan fathoni"[0])
+	fmt.Println("fauzan fathoni khorul"[1])
+	fmt.Println("fauzan fathoni khorul huda"[3])
+}

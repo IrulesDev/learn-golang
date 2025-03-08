4c65225895a717b1f5f1902c7534c172bf5d2fc0
IrulesDev <ad4112ul@gmail.com>
2025-03-08 07:20:17 +0700

    tambah cons+variable

---
+++ const.go
@@ -0,0 +1,10 @@
+package main
+
+func main(){
+	const fatherName string = "sukiman"
+	const motherName = "sularmi"
+
+	//error
+	// fatherName = "tidak bisa di ubah"
+	// motherName = "tidak bisa di ubah"
+}
---
+++ variable.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	name := "fauzan"
+	fmt.Println(name)
+
+	name = "fathoni"
+	fmt.Println(name)
+
+	var (
+		firstName = "Irules"
+		lastName = "Dev"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}

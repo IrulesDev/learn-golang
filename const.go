52ee2fbb5f883e19b73b3530f12c337d05343899
IrulesDev <ad4112ul@gmail.com>
2025-03-10 07:12:59 +0700

    fix:conversion.go

--- const.go
+++ const.go
@@ -1,10 +1,19 @@
 package main
 
+import "fmt"
+
 func main(){
-	const fatherName string = "sukiman"
-	const motherName = "sularmi"
+	// const fatherName string = "sukiman"
+	// const motherName = "sularmi"
 
 	//error
 	// fatherName = "tidak bisa di ubah"
 	// motherName = "tidak bisa di ubah"
+
+	const(
+		firsName = "irul"
+		lastName = "dev"
+	)
+
+	fmt.Println(firsName + lastName)
 }
